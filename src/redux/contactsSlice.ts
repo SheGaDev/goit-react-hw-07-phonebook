bab2b0737b33ec246db480b7d87a349d0a028f1f
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-10-11 20:55:31 +0300

    Update redux...

--- src/redux/contactsSlice.ts
+++ src/redux/contactsSlice.ts
@@ -1,29 +1,38 @@
-import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit';
-
-import { Contact, Form } from '@types';
+import { createSlice } from '@reduxjs/toolkit';
+import { addContact, deleteContact, fetchContacts } from './contactsThunk';
+import { initialState } from './initialState';
+import { CasePending, isPendingAction, isRejectedAction } from './cases';
+import { Contact } from '@types';
 
 export const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [] as Contact[],
-  reducers: {
-    addContact: {
-      reducer(state, action: PayloadAction<Contact>) {
-        state.push(action.payload);
-      },
-      prepare(form: Form) {
-        return {
-          payload: {
-            ...form,
-            id: nanoid(),
-          },
-        };
-      },
-    },
-    deleteContact: (state, action: PayloadAction<string>): Contact[] => {
-      return (state = [...state.filter((contact) => contact.id !== action.payload)]);
-    },
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = payload as Contact[];
+      })
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        const pl = payload as Contact;
+        state.isLoading = false;
+        state.error = null;
+        state.items = state.items.filter((contact) => contact.id !== pl.id);
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(payload as Contact);
+      })
+      .addCase(deleteContact.pending, CasePending)
+      .addMatcher(isPendingAction, CasePending)
+      .addMatcher(isRejectedAction, (state, action) => {
+        state.error = action.payload as string;
+        state.isLoading = false;
+      });
   },
 });
 
 export const contactsReducer = contactsSlice.reducer;
-export const { addContact, deleteContact } = contactsSlice.actions;
