e5f190a83c1bd13db3d5117831dee1fdc48f7a74
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-10-11 21:11:58 +0300

    fix reducers

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,19 +1,8 @@
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const rootConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['filter'],
-};
-
-export const combinedReducer = combineReducers({
+export const reducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
-
-export const reducer = persistReducer(rootConfig, combinedReducer);
