38526e65a75281e8393cee61f4d6cd8e84684e48
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-10-11 21:05:52 +0300

    delete redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import './global.css';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>
 );
